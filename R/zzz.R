b6ff043465f9591d522bb3bee5efc426a8fbac6b
matt-s-gibbs <gibbs.ms@gmail.com>
2023-05-23 17:15:47 +0930

    add citation information

---
+++ R/zzz.R
@@ -0,0 +1,23 @@
+SWTStartupMessage <- function()
+{
+  # Startup message obtained as 
+  # > figlet -f slant MCLUST
+  msg <- c(paste0("SWTools version ", 
+    packageVersion("SWTools")),
+    "\nIf you find SWTools useful please consider citing:\n
+    Gibbs M.S, Alcorn M. & Vaze, J. (2023) The SWTools R package for SILO
+    data acquisition, homogeneity testing and correction, 
+    Australasian Journal of Water Resources, DOI: 10.1080/13241583.2023.2214989
+    \nor type: citation(\"SWTools\")")
+  return(msg)
+}
+
+.onAttach <- function(lib, pkg)
+{
+  # unlock .mclust variable allowing its modification
+ # unlockBinding(".mclust", asNamespace("mclust")) 
+  # startup message
+  msg <- SWTStartupMessage()
+  packageStartupMessage(msg)      
+  invisible()
+}
